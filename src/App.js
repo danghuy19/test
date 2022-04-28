59b76ed7899d250db6b8eabd6ed5e2430879e54d
danghuy19 <ledanghuy199@gmail.com>
2022-04-28 21:04:02 +0700

    28/4 Commit

--- src/App.js
+++ src/App.js
@@ -9,85 +9,87 @@ import Poster from "./components/Poster";
 import Xbox from "./components/Xbox";
 import Footer from "./components/Footer";
 import Copywrite from "./components/Copywrite";
+import RegisterForm from "./components/RegisterForm";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { flag: 0, position_x: 10, position_y: 10 };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { flag: 0, position_x: 10, position_y: 10 };
+  // }
 
-  onHold = () => {
-    this.setState({
-      flag: 1,
-      position_x: this.state.position_x,
-      position_x: this.state.position_x,
-    });
-  };
+  // onHold = () => {
+  //   this.setState({
+  //     flag: 1,
+  //     position_x: this.state.position_x,
+  //     position_x: this.state.position_x,
+  //   });
+  // };
 
-  onDrag = (e) => {
-    if (this.state.flag) {
-      console.log("Keo");
-      // let vi_tri_chua = e.target.offsetLeft;
+  // onDrag = (e) => {
+  //   if (this.state.flag) {
+  //     console.log("Keo");
+  //     // let vi_tri_chua = e.target.offsetLeft;
 
-      let maximumPosX = 800;
-      let maximumPosY = 800;
+  //     let maximumPosX = 800;
+  //     let maximumPosY = 800;
 
-      let position_cursor_x = e.clientX - 550;
-      let position_cursor_y = e.clientY - 100;
+  //     let position_cursor_x = e.clientX - 550;
+  //     let position_cursor_y = e.clientY - 100;
 
-      if (
-        position_cursor_x >= maximumPosX &&
-        position_cursor_y >= maximumPosY
-      ) {
-        this.setState({ flag: 1, position_x: maximumPosX, position_y: maximumPosY });
-      }
+  //     if (
+  //       position_cursor_x >= maximumPosX &&
+  //       position_cursor_y >= maximumPosY
+  //     ) {
+  //       this.setState({ flag: 1, position_x: maximumPosX, position_y: maximumPosY });
+  //     }
 
-      if (position_cursor_x >= maximumPosX) {
-        this.setState({ flag: 1, position_x: maximumPosX, position_y: position_cursor_y });
-      } else if (position_cursor_y >= maximumPosY) {
-        this.setState({ flag: 1, position_x: position_cursor_x, position_y: maximumPosY });
-      } else {
-        this.setState({
-          flag: 1,
-          position_x: position_cursor_x,
-          position_y: position_cursor_y,
-        });
-      }
+  //     if (position_cursor_x >= maximumPosX) {
+  //       this.setState({ flag: 1, position_x: maximumPosX, position_y: position_cursor_y });
+  //     } else if (position_cursor_y >= maximumPosY) {
+  //       this.setState({ flag: 1, position_x: position_cursor_x, position_y: maximumPosY });
+  //     } else {
+  //       this.setState({
+  //         flag: 1,
+  //         position_x: position_cursor_x,
+  //         position_y: position_cursor_y,
+  //       });
+  //     }
 
-      // if (position_cursor_x <= 0) {
-      //   this.setState({ flag: 1, position_x: 0, position_y: position_cursor_y });
-      // } else if (position_cursor_y <= 0) {
-      //   this.setState({ flag: 1, position_x: position_cursor_x, position_y: 0 });
-      // } else {
-      //   this.setState({
-      //     flag: 1,
-      //     position_x: position_cursor_x,
-      //     position_y: position_cursor_y,
-      //   });
-      // }
-    }
-  };
+  //     // if (position_cursor_x <= 0) {
+  //     //   this.setState({ flag: 1, position_x: 0, position_y: position_cursor_y });
+  //     // } else if (position_cursor_y <= 0) {
+  //     //   this.setState({ flag: 1, position_x: position_cursor_x, position_y: 0 });
+  //     // } else {
+  //     //   this.setState({
+  //     //     flag: 1,
+  //     //     position_x: position_cursor_x,
+  //     //     position_y: position_cursor_y,
+  //     //   });
+  //     // }
+  //   }
+  // };
 
-  onDrop = () => {
-    this.setState({
-      flag: 0,
-      position_x: this.state.position_x,
-      position_x: this.state.position_x,
-    });
-  };
+  // onDrop = () => {
+  //   this.setState({
+  //     flag: 0,
+  //     position_x: this.state.position_x,
+  //     position_x: this.state.position_x,
+  //   });
+  // };
 
   render() {
     return (
       <div className="App">
-        {/* <TopBanner />
+        <TopBanner />
       <Slider />
       <Content />
       <Lastest />
       <Poster />
       <Xbox />
+      <RegisterForm />
       <Footer />
-      <Copywrite /> */}
-        <div className="object1" onMouseMove={this.onDrag}>
+      <Copywrite />
+        {/* <div className="object1" onMouseMove={this.onDrag}>
           <div
             className="object2"
             onMouseDown={this.onHold}
@@ -97,7 +99,7 @@ export default class App extends Component {
               left: this.state.position_x + "px",
             }}
           ></div>
-        </div>
+        </div> */}
       </div>
     );
   }
---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,171 @@
+import React, { Component } from "react";
+import "./css/RegisterForm.css"
+
+export default class RegisterForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: "",
+            password: "",
+            rePassword: "",
+            fullname: "",
+            phone: "",
+            address: "",
+            gender: "Male",
+            validEmail: null,
+            messageValidEmail: "",
+            validPassword: null,
+            messageValidPassword: ""
+        }
+    }
+
+    onChange = (e) => {
+        this.setState(prevState => {
+            prevState[e.target.name] = e.target.value
+            return prevState
+        })
+        console.log(this.state)
+    }
+
+    onKeyUp = (e) => {
+        if (e.target.name == "email") {
+            var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (re.test(e.target.value)) {//true
+                this.setState(prevState => {
+                    prevState.validEmail = true
+                    prevState.messageValidEmail = ""
+                })
+            } else {//false
+                this.setState(prevState => {
+                    prevState.validEmail = false
+                    prevState.messageValidEmail = "Invalid Email"
+                })
+            }
+        }
+
+        if (e.target.name == "password") {
+            if (e.target.value.length > 8 && e.target.value.length < 16) {
+                this.setState(prevState => {
+                    prevState.validPassword = true
+                    prevState.messageValidPassword = ""
+                })
+            }else {
+                this.setState(prevState => {
+                    prevState.validPassword = false
+                    prevState.messageValidPassword = "Invalid Password"
+                })
+            }
+        }
+    }
+
+  render() {
+    return (
+      <div className="container text-left">
+          <h2>Register Form</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              className={"form-control " + this.state.validEmail}
+              id="email"
+              name="email"
+              placeholder="Enter email"
+              value={this.state.email}
+              onChange={this.onChange}
+              onKeyUp={this.onKeyUp}
+            />
+            <small className={"form-text text-muted " + this.state.validEmail}>
+              {this.state.messageValidEmail}
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className={"form-control " + this.state.validPassword} 
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.onChange}
+              onKeyUp={this.onKeyUp}
+            />
+            <small className={"form-text text-muted " + this.state.validPassword}>
+              {this.state.messageValidPassword}
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="re-password">Re-Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="re-password"
+              name="rePassword"
+              placeholder="Re-Password"
+              value={this.state.rePassword}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="fullname">Full name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="fullname"
+              name="fullname"
+              placeholder="Full Name"
+              value={this.state.fullname}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="text"
+              className="form-control"
+              id="phone"
+              name="phone"
+              placeholder="Phone"
+              value={this.state.phone}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="adress">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              name="address"
+              placeholder="Address"
+              value={this.state.address}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="gender">Gender</label>
+            <select className="form-control" id="gender" name="gender" defaultValue={this.state.gender} onChange={this.onChange}>
+              <option>Male</option>
+              <option>Female</option>
+              <option>Other</option>
+            </select>
+          </div>
+          <div className="form-group form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
