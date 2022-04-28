1b01d8ce6ee5628534c9002a26a754bbf0f7d171
danghuy19 <ledanghuy199@gmail.com>
2022-04-28 21:29:21 +0700

    28/4 commit

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,68 +1,100 @@
 import React, { Component } from "react";
-import "./css/RegisterForm.css"
+import "./css/RegisterForm.css";
 
 export default class RegisterForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            email: "",
-            password: "",
-            rePassword: "",
-            fullname: "",
-            phone: "",
-            address: "",
-            gender: "Male",
-            validEmail: null,
-            messageValidEmail: "",
-            validPassword: null,
-            messageValidPassword: ""
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      rePassword: "",
+      fullname: "",
+      phone: "",
+      address: "",
+      gender: "Male",
+      validEmail: null,
+      messageValidEmail: "",
+      validPassword: null,
+      messageValidPassword: "",
+    };
+  }
+
+  onChange = (e) => {
+    this.setState((prevState) => {
+      prevState[e.target.name] = e.target.value;
+      return prevState;
+    });
+    console.log(this.state);
+  };
+
+  onKeyUp = (e) => {
+    if (e.target.name == "email") {
+      var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (re.test(e.target.value)) {
+        //true
+        this.setState((prevState) => {
+          prevState.validEmail = true;
+          prevState.messageValidEmail = "";
+        });
+      } else {
+        //false
+        this.setState((prevState) => {
+          prevState.validEmail = false;
+          prevState.messageValidEmail = "Invalid Email";
+        });
+      }
     }
 
-    onChange = (e) => {
-        this.setState(prevState => {
-            prevState[e.target.name] = e.target.value
-            return prevState
-        })
-        console.log(this.state)
+    if (e.target.name == "password") {
+      if (e.target.value.length > 8 && e.target.value.length < 16) {
+        this.setState((prevState) => {
+          prevState.validPassword = true;
+          prevState.messageValidPassword = "";
+        });
+      } else {
+        this.setState((prevState) => {
+          prevState.validPassword = false;
+          prevState.messageValidPassword = "Invalid Password";
+        });
+      }
+    }
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.validEmail && this.state.validPassword) {
+      alert("Valid");
+    } else {
+      alert("InValid");
     }
 
-    onKeyUp = (e) => {
-        if (e.target.name == "email") {
-            var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (re.test(e.target.value)) {//true
-                this.setState(prevState => {
-                    prevState.validEmail = true
-                    prevState.messageValidEmail = ""
-                })
-            } else {//false
-                this.setState(prevState => {
-                    prevState.validEmail = false
-                    prevState.messageValidEmail = "Invalid Email"
-                })
-            }
-        }
-
-        if (e.target.name == "password") {
-            if (e.target.value.length > 8 && e.target.value.length < 16) {
-                this.setState(prevState => {
-                    prevState.validPassword = true
-                    prevState.messageValidPassword = ""
-                })
-            }else {
-                this.setState(prevState => {
-                    prevState.validPassword = false
-                    prevState.messageValidPassword = "Invalid Password"
-                })
-            }
-        }
+    if (this.state.validEmail) {
+      // do nothing
+    } else {
+      this.setState((prevState) => {
+        prevState.validEmail = false;
+        prevState.messageValidEmail = "This field is required";
+        return prevState
+      });
     }
+
+    if (this.state.validPassword) {
+        // do nothing
+      } else {
+        this.setState((prevState) => {
+          prevState.validPassword = false;
+          prevState.messageValidPassword = "This field is required";
+          return prevState
+        });
+      }
+  };
 
   render() {
     return (
       <div className="container text-left">
-          <h2>Register Form</h2>
-        <form>
+        <h2>Register Form</h2>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label htmlFor="email">Email address</label>
             <input
@@ -83,7 +115,7 @@ export default class RegisterForm extends Component {
             <label htmlFor="password">Password</label>
             <input
               type="password"
-              className={"form-control " + this.state.validPassword} 
+              className={"form-control " + this.state.validPassword}
               id="password"
               name="password"
               placeholder="Password"
@@ -91,7 +123,9 @@ export default class RegisterForm extends Component {
               onChange={this.onChange}
               onKeyUp={this.onKeyUp}
             />
-            <small className={"form-text text-muted " + this.state.validPassword}>
+            <small
+              className={"form-text text-muted " + this.state.validPassword}
+            >
               {this.state.messageValidPassword}
             </small>
           </div>
@@ -145,7 +179,13 @@ export default class RegisterForm extends Component {
           </div>
           <div className="form-group">
             <label htmlFor="gender">Gender</label>
-            <select className="form-control" id="gender" name="gender" defaultValue={this.state.gender} onChange={this.onChange}>
+            <select
+              className="form-control"
+              id="gender"
+              name="gender"
+              defaultValue={this.state.gender}
+              onChange={this.onChange}
+            >
               <option>Male</option>
               <option>Female</option>
               <option>Other</option>
